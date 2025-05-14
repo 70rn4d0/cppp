b5f3121d4c84dcfb11d01d5b125e9ec31fa2196e
70rn4d0 <111705437+70rn4d0@users.noreply.github.com>
2025-05-14 06:00:16 +0000

    cpp01

---
+++ learn/learn.cpp
@@ -0,0 +1,16 @@
+#include "learn.hpp"
+
+
+int main()
+{
+    std::string one, two;
+    getline(std::cin, one);
+    std::cin >> two;
+    std::cout << one << "\n";
+
+
+    std::cout << two << "\n";
+
+
+
+}
---
+++ learn/learn.hpp
@@ -0,0 +1,8 @@
+#ifndef LEARN_H
+#define LEARN_H
+
+#include <iostream>
+
+int a = 5;
+
+#endif
